af2ef6a9acbe187bcf36e7f219a7ffeacf494979
LaiYuLong <yulong.lai.5@gmail.com>
2021-03-20 20:25:17 +0800

    testing API controller

---
+++ app/Http/Controllers/API_TestingController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class API_TestingController extends Controller
+{
+    public function Testing(){
+        echo "asdfasdf";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::get('/v1/test',[App\Http\Controllers\API_TestingController::class,'Testing']);
